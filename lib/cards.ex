ecc9dabc70506642418411513b7e0c15b25a6f31
NathanSinna <nathan.sinnathurai@accedo.tv>
2024-09-23 12:27:23 -0400

    add functions: deal, save_deck, load_deck

--- lib/cards.ex
+++ lib/cards.ex
@@ -8,7 +8,22 @@ defmodule Cards do
   Creates the deck
   """
   def create_deck do
-    values = ["Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"]
+    values = [
+      "Ace",
+      "King",
+      "Queen",
+      "Jack",
+      "Ten",
+      "Nine",
+      "Eight",
+      "Seven",
+      "Six",
+      "Five",
+      "Four",
+      "Three",
+      "Two"
+    ]
+
     suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
 
     for suit <- suits, value <- values, do: "#{value} of #{suit}"
@@ -26,5 +41,30 @@ defmodule Cards do
   """
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  @doc """
+  Deals a hand from the deck
+  """
+  def deal(deck, n) do
+    Enum.split(deck, n)
+  end
+
+  @doc """
+  Saves the deck to file system
+  """
+  def save_deck(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write!(filename, binary)
+  end
+
+  @doc """
+  Loads the deck a saved file.
+  """
+  def load_deck(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "This file does not exist!"
+    end
   end
 end
