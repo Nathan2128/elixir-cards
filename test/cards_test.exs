8206c5fb75acec2622df7d212c1503f2eaf44d68
NathanSinna <nathan.sinnathurai@accedo.tv>
2024-09-25 21:02:03 -0400

    adding another test case

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -5,4 +5,11 @@ defmodule CardsTest do
   test "creates deck of 52 cards" do
     assert length(Cards.create_deck()) == 52
   end
+
+  test "shuffling a deck gives us random order" do
+    deck = Cards.create_deck()
+    shuffled_deck = Cards.shuffle_deck(deck)
+
+    refute deck == shuffled_deck
+  end
 end
